7c7dded67d4e3838867ba9813ecd5352584e27b4
Lorenzo Benatti <benzo2000@tiscali.it>
2022-05-25 11:59:22 +0200

    Fetch series

--- moduli/modulo-api.js
+++ moduli/modulo-api.js
@@ -1,5 +1,8 @@
 const URL_LIST_AVENGERS =
   "http://www.omdbapi.com/?apikey=46a5f494&s=Avengers"
+  /*
+   Chiama la web api con l'elenco di tutti i contenuti di "Avengers"
+   */
 
   export const listAvengers = () => {
       fetch(URL_LIST_AVENGERS)
@@ -9,3 +12,19 @@ const URL_LIST_AVENGERS =
           console.log(movies);
       });
   };
+
+  /*
+Mettere la chiamata alla funzione per l'elenco della serie televisive "Avengers"
+   */
+
+const URL_LIST_AVENGERSSERIES =
+  "http://www.omdbapi.com/?apikey=46a5f494&s=Avengers&type=series"
+
+  export const listAvengersSeries = () => {
+    fetch(URL_LIST_AVENGERSSERIES)
+    .then((response) => response.json())
+    .then((results) => {
+        const series = results.Search;
+        console.log(series);
+    });
+};
